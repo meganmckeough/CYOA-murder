10c53ca0f30b60b221a2f6a4d99f49202a6e7b85
Megan McKeough <megan.mckeough@gmail.com>
2018-07-25 11:17:16 +1000

    added to char creation

--- app_console.rb
+++ app_console.rb
@@ -1,3 +1,5 @@
 require 'pry'
 require_relative 'db_config'
+require_relative 'models/character'
+require_relative 'models/user'
 binding.pry
--- main.rb
+++ main.rb
@@ -21,11 +21,16 @@ get '/' do
 end
 
 get '/character' do
+	redirect '/login' unless logged_in?
 	erb :character
 end
 
 get '/about' do
 	erb :about
+end
+
+get '/story' do
+	erb :story
 end
 
 enable :sessions
@@ -52,9 +57,20 @@ end
 post '/character/new' do
 	character = Character.new
 	character.name = params[:name]
+	character.save
+	redirect '/character/edit'
+end
+
+get '/character/edit' do
+	@character = Character.last
+	erb :background
+end
+
+put '/character/:id/edit' do
+	character = Character.find(params[:id])
 	character.background = params[:background]
 	character.save
-	redirect '/'
+	redirect "/story"
 end
 
 post '/session' do
