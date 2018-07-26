7a2a1fbc213aa69cc9c12716ed67ae106b5f2245
Megan McKeough <megan.mckeough@gmail.com>
2018-07-26 10:42:22 +1000

    data update

--- murder_mystery.sql
+++ murder_mystery.sql
@@ -17,8 +17,6 @@ CREATE TABLE characters (
 
 INSERT INTO users (name, email, password) values ('megan', 'megan.mckeough@gmail.com', 'pudding');
 
-UPDATE users SET password = 'pudding' WHERE id = 1;
-
 ALTER TABLE characters ADD created_at TIMESTAMP;
 
 ALTER TABLE characters DROP created_at;
@@ -33,3 +31,5 @@ CREATE TABLE segments (
 );
 
 
+
+UPDATE segments SET content='You enter the conservatory with apprehension, but there is nothing but a dark, glass-walled room - empty. You decide to take a look around anyway. It is not long before you find it. A long, thin letter opener, hidden in a pot plant. As the rain lashes the roof of the conservatory, you reach down and grasp the handle. You are just examining the engraved initials ("J.A") when someone clears their throat behind you. You whip around and are confronted with a tall man in round glasses and a neat moustache. Your hand tightens around the letter opener as your instincts kick in.  You quickly lash out with the letter opener, slashing his cheek and splashing blood on his butler uniform. While he is stunned, you push him aside and run for the door. Once in the hallway, you block the door from the outside to prevent his escape.' WHERE story_id='32';
